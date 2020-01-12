126e69274784b35c1cf4879ec35d3da435bdb67e
Mateusz Piotrowski <0mp@FreeBSD.org>
2020-01-12 23:56:17 +0100

    Add my old config.h changes

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -11,15 +13,16 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_green[]       = "#00775d";
+static const char col_yellow[]      = "#ffff00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_green, col_yellow },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "q", "w", "e", "r", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,7 +31,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -44,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,8 +58,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *volupcmd[]  = { "mixer", "vol", "+2", NULL };
+static const char *voldowncmd[]  = { "mixer", "vol", "-2", NULL };
+static const char *volmutecmd[]  = { "mixer", "vol", "0", NULL };
+static const char *plumbselectioncmd[] = { "sh", "-c", "sh ~/.suckless/plumb.sh \"$(xclip -o)\"", NULL };
+static const char *findcursorcmd[] = { "find-cursor", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -88,12 +95,17 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_q,                      4)
+	TAGKEYS(                        XK_w,                      5)
+	TAGKEYS(                        XK_e,                      6)
+	TAGKEYS(                        XK_r,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|Mod1Mask,  XK_q,      quit,           {0} },
+	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = volupcmd} },
+	{ 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = voldowncmd} },
+	{ 0,         XF86XK_AudioMute,             spawn,          {.v = volmutecmd} },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = plumbselectioncmd} },
+	{ MODKEY,                       XK_less,   spawn,          {.v = findcursorcmd} },
 };
 
 /* button definitions */
